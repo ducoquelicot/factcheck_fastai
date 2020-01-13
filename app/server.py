cef9287b7a4b0de2d25472dcc269e908316cd6bd
Fabienne Meijer <47226700+ducoquelicot@users.noreply.github.com>
2020-01-13 17:49:53 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -68,7 +68,8 @@ def slack_this(data, url):
     status = {
         # 'quartz': r.status_code,
         # 'statesman': p.status_code,
-        'vrtnws': v.status_code
+        'vrtnws': v.status_code,
+        'output': slack_webhook_url
     }
     
     return status
